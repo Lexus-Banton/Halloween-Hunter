085fc2cc8ba9947873dfde0fdabbee269e7f7516
Lexus Banton <lexusbanton.kr@gmail.com>
2022-09-21 12:34:25 -0700

    netlify link push

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 import { renderMonster } from './render-utils.js';
 /* Get DOM Elements */
 const monsterSection = document.getElementById('monster-section');
+const deadMonsters = document.getElementById('dead-monsters');
+const damageDone = document.getElementById('damage-done');
 /* State */
 let monsters = [
     {
@@ -30,5 +32,16 @@ function displayMonsters() {
         monsterSection.append(monsterEl);
     }
 }
+
+function displayDamage() {
+    damageDone.textContent = `You hit ${monsters.name} and  ${0} damage`;
+}
+function displayDead() {
+    deadMonsters.textContent = `You have Killed ${0} monsters`;
+}
 displayMonsters();
+displayDamage();
+displayDead();
+
 // (don't forget to call any display functions you want to run on page load!)
+//test
